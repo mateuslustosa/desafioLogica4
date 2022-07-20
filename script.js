c8d6dfd28736b72993ff9d85845c8efc11e9d37c
mateuslustosa <mateuslustosa.o@gmail.com>
2022-07-20 13:52:15 -0300

    Desafio 4 completo!

---
+++ script.js
@@ -0,0 +1,24 @@
+//Crie uima função que exiba uma mensagem no console
+ function mensagem(){
+        console.log("Olá");
+ }
+ mensagem();
+
+//Crie uma function que receba o seu nome como parâmetro e exiba no console
+ function nome(name){
+   console.log(`Bem-vindo, ${name}!`);
+ }
+ nome();
+
+//Crie uma função que receba nome, idade, e estilo musical preferido (parâmetros) e exiba no console
+ function about(name, age, music){
+   console.log(`Meu nome é: ${name}, tenho ${age} anos e gosto de ouvir ${music}`);
+ }
+ about("Mateus", 26, "pagode");
+
+//Crie uma função que receba um filme, uma musica (parâmetros) e exiba no console
+ function artes(filme, musica){
+   console.log(`Meu filme favorito é: ${filme} e minha música favorita é: ${musica}`);
+ }
+
+ artes("As Branquelas", "CBJR");
